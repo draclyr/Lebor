219c175a8bacda81efab5ab841e9bba5feb449f6
Draclyr ( Hesam Shahmoradi ) <draclyr@gmail.com>
2025-04-26 16:48:31 -0700

    poject title revision

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
 
             <div className="relative flex place-items-center my-40 before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] flex-col">
                 <h1 className="text-2xl font-bold text-center text-primary">
-                    Im Hesam Shahmoradi <Highlight>DRACLYR</Highlight>
+                    Hesam Shahmoradi <Highlight>DRACLYR</Highlight>
                 </h1>
                 <p className="text-center text-lg max-w-[500px] mt-5">
                     Welcome To My Portfolio.
